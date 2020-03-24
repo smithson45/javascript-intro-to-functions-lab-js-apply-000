c291877b84fe5b0caf27f139028a6749b520f2a2
Andre Carter <carstar2486@gmail.com>
2020-03-24 02:22:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   var lowercase = 'Hello';
-  lowercase.toLowerCase() === lowercase;)
+  lowercase.toLowerCase() === lowercase;
   return lowercase;
   
 }
